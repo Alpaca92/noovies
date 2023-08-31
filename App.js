aa19858ae5bbda6891a271e88bf02a2e8bc924f3
ayaan <ayaan@t-win.kr>
2023-08-31 10:22:44 +0900

    #2.3 AppLoading part Three (11:16) (1)

--- App.js
+++ App.js
@@ -7,6 +7,16 @@ import { Text, View } from 'react-native';
 
 SplashScreen.preventAutoHideAsync();
 
+const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
+
 export default function App() {
   const [ready, setReady] = useState(false);
 
@@ -15,7 +25,12 @@ export default function App() {
       try {
         // pre-load fonts, call APIs, etc
         // 강의의 startLoading과 동일하게 동작
-        await Font.loadAsync(Ionicons.font);
+        const fonts = loadFonts([Ionicons.font]);
+        const images = loadImages([
+          /* image url 혹은 require */
+        ]);
+
+        await Promise.all([...fonts, ...images]);
       } catch (error) {
         // 강의의 onError와 동일하게 동작
         console.warn(error);
