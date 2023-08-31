67c7c594ec38a4536837a0f383b2c46b365f126e
ayaan <ayaan@t-win.kr>
2023-08-31 13:54:06 +0900

    Migrate the Nomadcoders React Native course : #2.5 Tab Navigation (08:19)

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import React, { useCallback } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
 import { Ionicons } from '@expo/vector-icons';
 import { useAssets } from 'expo-asset';
-import { Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
+import { NavigationContainer } from '@react-navigation/native';
+import Tabs from './navigation/Tabs';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -22,8 +23,8 @@ export default function App() {
   }
 
   return (
-    <View onLayout={onLayoutRootView}>
-      <Text>We are done Loading !</Text>
-    </View>
+    <NavigationContainer onReady={onLayoutRootView}>
+      <Tabs />
+    </NavigationContainer>
   );
 }
