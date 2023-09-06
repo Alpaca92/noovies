90ff77f268eb711dd23a43e637b0eb685250225d
ayaan <ayaan@t-win.kr>
2023-09-06 13:02:01 +0900

    #3.3 Movies Screen part Two (17:17)

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -19,6 +19,9 @@ const Tabs = () => {
 
   return (
     <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: isDark ? BLACK_COLOR : WHITE_COLOR,
+      }}
       screenOptions={{
         tabBarStyle: {
           backgroundColor: isDark ? BLACK_COLOR : WHITE_COLOR,
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,54 +1,79 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
 import Swiper from 'react-native-web-swiper';
-import { Dimensions } from 'react-native';
+import { ActivityIndicator, Dimensions, StyleSheet } from 'react-native';
+import { makeImagePath } from '../utils';
+import { BlurView } from 'expo-blur';
 
-const ScrollView = styled.ScrollView`
-  background-color: ${({ theme }) => theme.mainBgColor};
-`;
+const ScrollView = styled.ScrollView``;
 
 const View = styled.View`
   flex: 1;
 `;
+
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BgImg = styled.Image``;
+
+const Title = styled.Text``;
 
 const { height: SCREEN_HIGHT } = Dimensions.get('window');
 
 const Movies: React.FC<NativeStackScreenProps<any, 'Movies'>> = ({
   navigation: { navigate },
 }) => {
-  const getNowPlaying = () => {
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getNowPlaying = async () => {
     const url =
       'https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1';
     const options = {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: 'Bearer "SOME_API_TOKEN"', // "SOME_API_TOKEN" 부분에 실제 토큰이 들어가야 함
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZmI5Yjc5YWI1MWYxZTNkNjgyMGZkZDVmZWRhNmEzZSIsInN1YiI6IjYyMGRlNDNkZjc5NGFkMDA0MjQzMGVkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iq3ANuPSRpYIYQgtlPGAp0QjCaooNrrNdqhw3gZjyw8',
       },
     };
 
-    const result = fetch(url, options)
-      .then((res) => res.json())
-      .then((json) => console.log(json))
-      .catch((err) => console.error('error:' + err));
+    const { results } = await (await fetch(url, options)).json();
 
-    console.log(result);
+    setNowPlayingMovies(results);
+    setLoading(false);
   };
 
-  getNowPlaying();
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
 
-  return (
+  return loading ? (
+    <Loader>
+      <ActivityIndicator />
+    </Loader>
+  ) : (
     <ScrollView>
       <Swiper
         containerStyle={{ width: '100%', height: SCREEN_HIGHT / 4 }}
         loop
         controlsEnabled={false}
         timeout={3.5}>
-        <View style={{ backgroundColor: 'red' }}></View>
-        <View style={{ backgroundColor: 'blue' }}></View>
-        <View style={{ backgroundColor: 'green' }}></View>
-        <View style={{ backgroundColor: 'ivory' }}></View>
+        {nowPlayingMovies.map((movie) => (
+          <View key={movie.id}>
+            <BgImg
+              style={StyleSheet.absoluteFill}
+              source={{ uri: makeImagePath(movie.backdrop_path) }}
+            />
+            <BlurView style={StyleSheet.absoluteFill}>
+              <Title>{movie.original_title}</Title>
+            </BlurView>
+          </View>
+        ))}
       </Swiper>
     </ScrollView>
   );
